62f0c3b8328a7cbaf415b65728844447291fc655
James Dorrington <jdorrington@hotmail.com>
2019-12-02 23:23:28 +0000

    update

--- src/maths.ex
+++ src/maths.ex
@@ -7,6 +7,14 @@ defmodule M do
     maths_stuff()
   end
   
-  def maths_stuff do 
+  def maths_stuff do  
+    IO.puts "10 + 9 = #{10 + 9}"
+    IO.puts "10 - 9 = #{10 - 9}"
+    IO.puts "10 * 9 = #{10 * 9}"
+    IO.puts "10 / 9 = #{10 / 9}"
+    IO.puts "10 div 9 = #{div(10, 9)}"
+    IO.puts "10 rem 9 = #{rem(10, 9)}"
+    
+    # sin, cos, tan, log, e, sqrt, etc. etc. blah. blah. blah
   end
 end
